825ade209c5b7b189012346cab82c864fdf89e4a
Bhargavishiva <90924314+Bhargavishiva@users.noreply.github.com>
2021-09-28 16:35:10 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,172 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/config.php');
+?>
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>TMS | Tourism Management System</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<script type="applijewelleryion/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/bootstrap.css" rel='stylesheet' type='text/css' />
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href='//fonts.googleapis.com/css?family=Open+Sans:400,700,600' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Roboto+Condensed:400,700,300' rel='stylesheet' type='text/css'>
+<link href='//fonts.googleapis.com/css?family=Oswald' rel='stylesheet' type='text/css'>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@100&family=Spicy+Rice&display=swap" rel="stylesheet">
+<link href="css/font-awesome.css" rel="stylesheet">
+<!-- Custom Theme files -->
+<script src="js/jquery-1.12.0.min.js"></script>
+<script src="js/bootstrap.min.js"></script>
+<!--animate-->
+<link href="css/animate.css" rel="stylesheet" type="text/css" media="all">
+<script src="js/wow.min.js"></script>
+	<script>
+		 new WOW().init();
+	</script>
+<!--//end-animate-->
+</head>
+<body>
+<?php include('includes/header.php');?>
+<div class="banner">
+	<div class="container">
+	<img class="plane"src="airplane.png">
+		<p class=" travel wow zoomIn animated animated" data-wow-delay=".5s" style="visibility: visible; animation-delay: 0.5s; animation-name: zoomIn;">Travel is the only thing you buy that makes you richer </p>
+	</div>
+</div>
+
+
+
+<!--- rupes ---->
+<div class="waste">
+<div class="col-lg-4">
+<i class="fa fa-money" aria-hidden="true"></i>
+<h1>50% off</h1>
+<p>Travel tour</p>
+
+</div>
+<div class="col-lg-4">
+<i class="fa fa-h-square" aria-hidden="true"></i>
+<h1>70% off</h1>
+<p>On Hotels</p>
+
+</div>
+<div class="col-lg-4">
+<i class="fa fa-mobile" aria-hidden="true"></i>
+<h1>50% off</h1>
+<p>Festival offers</p>
+
+</div>
+
+
+
+
+</div>
+<!--- /rupes ---->
+
+
+
+
+<!---holiday---->
+<div class="container">
+	<div class="holiday">
+	
+
+
+
+	
+	<h3 class="ho">Package List</h3>
+
+					
+<?php $sql = "SELECT * from tbltourpackages order by rand() limit 4";
+$query = $dbh->prepare($sql);
+$query->execute();
+$results=$query->fetchAll(PDO::FETCH_OBJ);
+$cnt=1;
+if($query->rowCount() > 0)
+{
+foreach($results as $result)
+{	?>	<div class="pad">
+			<div class="rom-btm">
+			
+				<div class="col-md-3 room-left wow fadeInLeft animated" data-wow-delay=".5s">
+					<img src="admin/pacakgeimages/<?php echo htmlentities($result->PackageImage);?>" class="img-responsive" alt="">
+				</div>
+				<div class="col-md-6 room-midle wow fadeInUp animated" data-wow-delay=".5s">
+					<h4>Place Name: <?php echo htmlentities($result->PackageName);?></h4>
+					<h6>Place Type : <?php echo htmlentities($result->PackageType);?></h6>
+					<p><b>Location :</b> <?php echo htmlentities($result->PackageLocation);?></p>
+					<p><b>Facilities</b> <?php echo htmlentities($result->PackageFetures);?></p>
+				</div>
+				<div class="col-md-3 room-right wow fadeInRight animated" data-wow-delay=".5s">
+					<h5>USD <?php echo htmlentities($result->PackagePrice);?></h5>
+					<a href="package-details.php?pkgid=<?php echo htmlentities($result->PackageId);?>" class="view">Details</a>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+</div>
+
+<?php }} ?>
+			
+		
+<div><a href="package-list.php" class="view">View More Packages</a></div>
+</div>
+			<div class="clearfix"></div>
+	</div>
+
+
+
+<!--- routes ---->
+<div class="routes">
+	<div class="container">
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-info-circle" aria-hidden="true"></i></a>
+			</div>
+			<div class="rou-rgt wow fadeInDown animated" data-wow-delay=".5s">
+				<h3>20000</h3>
+				<p>Enquiries</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-user"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>800</h3>
+				<p>Regestered users</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="col-md-4 routes-left wow fadeInRight animated" data-wow-delay=".5s">
+			<div class="rou-left">
+				<a href="#"><i class="fa fa-ticket"></i></a>
+			</div>
+			<div class="rou-rgt">
+				<h3>10,00,00,000+</h3>
+				<p>Booking</p>
+			</div>
+				<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+
+<?php include('includes/footer.php');?>
+<!-- signup -->
+<?php include('includes/signup.php');?>			
+<!-- //signu -->
+<!-- signin -->
+<?php include('includes/signin.php');?>			
+<!-- //signin -->
+<!-- write us -->
+<?php include('includes/write-us.php');?>			
+<!-- //write us -->
+</body>
+</html>
